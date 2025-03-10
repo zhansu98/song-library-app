7121288cff32dc7b3c956d80b011103d35f229f3
Zhan <zhan.su@smoothstack.com>
2025-03-09 18:47:04 -0600

    Updated to sort using angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SongService } from './services/song.service';
 import { SongListComponent } from "./components/song-list/song-list.component";
 import { MockBackendService } from './services/mock-backend.service';
-import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,13 +24,21 @@ import { provideHttpClient } from '@angular/common/http';
     CommonModule,
     RouterOutlet,
     BrowserModule,
+    BrowserAnimationsModule,
+    //Materials
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule
 ],
 providers: [
   SongService,
   MockBackendService,
   CurrencyPipe,
   DatePipe,
-  provideHttpClient()
 ],
   bootstrap: [AppComponent]
 })
--- src/app/components/song-list/song-list.component.ts
+++ src/app/components/song-list/song-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { SongService } from '../../services/song.service';
 import { Song } from '../../models/song';
+import { MatTableDataSource } from '@angular/material/table';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-song-list',
@@ -8,10 +10,11 @@ import { Song } from '../../models/song';
   styleUrls: ['./song-list.component.css'],
   standalone: false
 })
-export class SongListComponent implements OnInit {
-  songs: Song[] = [];
-  private sortAttribute: string = 'title';
-  private sortDirection: 'asc' | 'desc' = 'asc';
+export class SongListComponent implements OnInit{
+  dataSource = new MatTableDataSource<Song>();
+  sortAttribute: keyof Song = 'title';
+  sortDirection: string = 'asc';
+  displayedColumns: string[] = ['title', 'artist', 'releaseDate', 'price', 'actions'];
 
   constructor(private songService: SongService) { }
 
@@ -21,26 +24,30 @@ export class SongListComponent implements OnInit {
 
   getSongs(): void {
     this.songService.getSongs().subscribe(songs => {
-      this.songs = songs;
-      this.sortSongs(this.sortAttribute);
+      this.dataSource.data = songs;
     });
   }
 
-  sortSongs(attribute: string): void {
-    this.sortAttribute = attribute;
-    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-    this.songs.sort((a, b) => {
-      const aValue = a[attribute];
-      const bValue = b[attribute];
+  sortData(sort: Sort): void {
+    const data = this.dataSource.data.slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSource.data = data;
+      return;
+    }
 
-      if (typeof aValue === 'string' && typeof bValue === 'string') {
-        return this.sortDirection === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
-      } else if (typeof aValue === 'number' && typeof bValue === 'number') {
-        return this.sortDirection === 'asc' ? aValue - bValue : bValue - aValue;
-      } else if (aValue instanceof Date && bValue instanceof Date) {
-        return this.sortDirection === 'asc' ? aValue.getTime() - bValue.getTime() : bValue.getTime() - aValue.getTime();
-      } else {
-        return 0;
+    this.dataSource.data = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'artist':
+          return compare(a.artist, b.artist, isAsc);
+        case 'releaseDate':
+          return compare(a.releaseDate, b.releaseDate, isAsc);
+        case 'price':
+          return compare(a.price, b.price, isAsc);
+        default:
+          return 0;
       }
     });
   }
@@ -49,5 +56,17 @@ export class SongListComponent implements OnInit {
     // this.songService.deleteSong(id).subscribe(() => {
     //   this.getSongs();
     // });
+  }
+}
+
+function compare(a: string | number | Date, b: string | number | Date, isAsc: boolean): number {
+  if (a instanceof Date && b instanceof Date) {
+    return (a.getTime() - b.getTime()) * (isAsc ? 1 : -1);
+  } else if (typeof a === 'number' && typeof b === 'number') {
+    return (a - b) * (isAsc ? 1 : -1);
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a.localeCompare(b) * (isAsc ? 1 : -1);
+  } else {
+    return 0;
   }
 }
